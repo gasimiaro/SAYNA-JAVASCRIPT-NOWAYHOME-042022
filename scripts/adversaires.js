8db70e4b069bece25864c6de457add04c8aa491e
gasimiaro <zagasimiaro@gmail.com>
2022-05-06 13:18:34 +0300

    adversaire terminer

--- scripts/adversaires.js
+++ scripts/adversaires.js
@@ -1,50 +1,30 @@
 $(document).ready(function() {
 
-    $('.card-image').hover(function() {
-        $(this).addClass('transition');
-    }, function() {
-        $(this).removeClass('transition');
-    });
-    // card-text content hiding
-    $('.card-content-1').hide()
-    $('.card-image-1').on({
+    $('.card-hero').on({
         mouseenter: function() {
-            $('.card-content-1').fadeTo(7000, 1)
-            $('.card-title').css({
-                'background-color': '#114b9298'
-            })
-            $('.triangle').hide(2000)
+            $(this).css({
+                "transform": "scale(1.05)",
+                "cursor": "pointer",
+                "transition": " all 0.5s ease-in-out",
+                "color": "var(--white-ad-color)",
+                "background-color": "var(--black-ad-color)"
 
+            })
         },
         mouseleave: function() {
-            $('.card-content-1').hide()
-            $('.card-title').css({
-                'background-color': 'rgba(0, 0, 0, 0.7)'
-            })
-            $('.triangle').show()
+            $(this).css({
+                "transform": "scale(1)",
+                "cursor": "pointer",
+                "transition": " all 0.5s ease-in-out",
+                "color": "var(--black-ad-color)",
+                "background-color": "var(--primary-ad-color)"
 
+            })
         }
     })
-    $('.card2').on({
-        mouseenter: function() {
-            $('.card-text2').fadeTo(1500, 1)
-        },
-        mouseleave: function() {
-            $('.card-text2').fadeOut(1000)
-        }
-    })
-    $('.card3').on({
-            mouseenter: function() {
-                $('.card-text3').fadeTo(1500, 1)
-                $('.card-text3').nextUntil('.button1').fadeTo(1500, 1)
-            },
-            mouseleave: function() {
-                $('.card-text3').fadeOut(1000)
-                $('.card-text3').nextUntil('.button1').fadeOut(1000)
-            }
-        })
-        // ****************************************************************************************************
-        // social media hover
+
+    // ****************************************************************************************************
+    // social media hover
     $('.mediahover').on({
             mouseenter: function() {
                 $(this).css('color', 'white')
